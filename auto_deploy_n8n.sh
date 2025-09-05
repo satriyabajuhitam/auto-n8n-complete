e75f70205434e085f43126e03dc9c1dc4aee1faf
Satriya Baju Hitam <satriyabajuhitam.id@gmail.com>
2025-09-05 12:42:17 +0700

    Update auto_deploy_n8n.sh

--- auto_deploy_n8n.sh
+++ auto_deploy_n8n.sh
@@ -838,7 +838,7 @@ services:
     environment:
       - N8N_HOST=0.0.0.0
       - N8N_PORT=5678
-      - N8N_PROTOCOL=http
+      - N8N_PROTOCOL=https
       - NODE_ENV=production
       - WEBHOOK_URL=http://localhost:5678/
       - GENERIC_TIMEZONE=Asia/Jakarta
@@ -886,7 +886,7 @@ services:
     environment:
       - N8N_HOST=0.0.0.0
       - N8N_PORT=5678
-      - N8N_PROTOCOL=http
+      - N8N_PROTOCOL=https
       - NODE_ENV=production
       - WEBHOOK_URL=https://${DOMAIN}/
       - GENERIC_TIMEZONE=Asia/Jakarta
