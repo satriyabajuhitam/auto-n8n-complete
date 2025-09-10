95212008beb56f9c46fec25df4ca034c05205c2b
Satriya Baju Hitam <satriyabajuhitam.id@gmail.com>
2025-09-10 17:10:01 +0700

    Update auto_deploy_n8n.sh

--- auto_deploy_n8n.sh
+++ auto_deploy_n8n.sh
@@ -1406,17 +1406,19 @@ services:
       N8N_ENCRYPTION_KEY: \${N8N_ENCRYPTION_KEY:-$(openssl rand -hex 32)}
       DB_TYPE: "sqlite"
       DB_SQLITE_DATABASE: "/home/node/.n8n/database.sqlite"
+      DB_SQLITE_POOL_SIZE: "1"
       N8N_BASIC_AUTH_ACTIVE: "false"
       N8N_DISABLE_PRODUCTION_MAIN_PROCESS: "false"
       EXECUTIONS_TIMEOUT: "3600"
       EXECUTIONS_TIMEOUT_MAX: "7200"
       N8N_EXECUTIONS_DATA_MAX_SIZE: "500MB"
-      N8N_BINARY_DATA_TTL: "1440"
       N8N_BINARY_DATA_MODE: "filesystem"
       N8N_BINARY_DATA_STORAGE: "/files"
       N8N_DEFAULT_BINARY_DATA_FILESYSTEM_DIRECTORY: "/files"
       N8N_DEFAULT_BINARY_DATA_TEMP_DIRECTORY: "/files/temp"
       NODE_FUNCTION_ALLOW_BUILTIN: "child_process,path,fs,util,os"
+      N8N_RUNNERS_ENABLED: "true"
+      N8N_BLOCK_ENV_ACCESS_IN_NODE: "false"
     volumes:
       - ./files:/home/node/.n8n
       - ./files/youtube_content_anylystic:/data/youtube_content_anylystic
